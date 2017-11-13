ac51b1dbcd6a75c0eb4a7cd9e6a481e35bc3d7d6
AlexAegis <alexaegis@gmail.com>
2017-11-13 23:42:41 +0100

    06

--- 06-iterator/List.h
+++ 06-iterator/List.h
@@ -1,33 +1,65 @@
+//
+// Created by alexa on 2017-11-13.
+//
+
 #ifndef INC_06_ITERATOR_LIST_H
 #define INC_06_ITERATOR_LIST_H
+#include <iostream>
 
 template <class T>
 class List {
 private:
-public:
+
     T data{};
     List<T>* next = nullptr;
     int count = 1;
 
+public:
 
     List(T data) : data(data), next(nullptr) {}
     List(T data, int count) : data(data), count(count), next(nullptr) {}
 
-    ~List();
+    ~List() {
+        delete next;
+    }
 
-    void add(T _data);
+    void add(T _data) {
+        add(_data, count);
+    }
 
-    void add(T _data, int currentCount);
+    void add(T _data, int currentCount) {
+        if(next == nullptr) {
+            next = new List<T>(_data, currentCount + 1);
+        } else {
+            next->add(_data, currentCount + 1);
+        }
+    }
 
-    List<T>* getLastNode();
+    List<T>* getLastNode() {
+        if(next == nullptr) {
+            return this;
+        } else next->getLastNode();
+    }
 
-    int size();
+    int size() {
+        return getLastNode()->count;
+    }
 
-    T get();
+    T get() {
+        return data;
+    }
 
-    T get(int i);
+    T get(int i) {
+        if(count == i) {
+            return data;
+        } else {
+            next->get(i);
+        }
+    }
 
-    List<T>* getNext();
+    List<T>* getNext() {
+        return next;
+    }
 
 };
 
